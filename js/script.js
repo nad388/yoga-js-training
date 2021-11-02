7edf0fe39cecba6c80b4a9b468f6acba76c4db19
Nadezhda <nad388@yandex.ru>
2021-11-02 15:39:04 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -173,4 +173,51 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     sendForm(form);
     sendForm(formButton);
+
+    // SLIDER
+
+    let slideIndex = 1, // параметр текущего слайда
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    // скрываем слайды, кроме первого
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach((item) => item.style.display = 'none'); // перебираем слайды и скрваем их.
+        dots.forEach((item) => item.classList.remove('dot-active'));
+        slides[slideIndex - 1].style.display = 'block';// слайд, который хотим показать. Конвертируем нормальную нумерацию слайдов в нумерауию js.
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function () {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', function () {
+        plusSlides(1);
+    });
+    dotsWrap.addEventListener('click', function (event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i);
+            }
+        }
+    });
+
 });
